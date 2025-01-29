80e746d90a19e6a4e3852151a92629a2d6afb0d3
Leonid Bugaenko <bugaenko.leonid@gmail.com>
2025-01-29 10:57:39 +0400

    improved content generation and image handling

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -12,23 +12,41 @@ class ArticleFactory extends Factory
 
     public function definition(): array
     {
-        $this->faker->locale('en_US');
+        // Создаем фейкер с английской локалью
+        $faker = \Faker\Factory::create('en_US');
         
-        $title = $this->faker->unique()->sentence(3);
+        // Генерируем технический заголовок на английском
+        $title = $faker->realText(60);
+        $title = ucfirst(trim(preg_replace('/[.!?]$/', '', $title)));
+        
+        // Генерируем содержимое из нескольких параграфов
+        $paragraphs = [];
+        $paragraphCount = $faker->numberBetween(3, 7);
+        
+        for ($i = 0; $i < $paragraphCount; $i++) {
+            $paragraphs[] = $faker->realText($faker->numberBetween(200, 500));
+        }
         
         return [
             'title' => $title,
             'slug' => Str::slug($title),
-            'content' => collect($this->faker->paragraphs(5))
-                ->map(fn($paragraph) => "<p>{$paragraph}</p>")
-                ->join(''),
-            'image' => null,
-            'views_count' => $this->faker->numberBetween(0, 1000),
-            'likes_count' => $this->faker->numberBetween(0, 100),
-            'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'content' => implode("\n", $paragraphs),
+            'image' => null, // изначально null
+            'views_count' => 0,
+            'likes_count' => 0,
+            'created_at' => now(),
             'updated_at' => function (array $attributes) {
                 return $attributes['created_at'];
             },
         ];
+    }
+
+
+    //присваивает картинке id статьи
+    public function configure()
+    {
+        return $this->afterCreating(function (Article $article) {
+            $article->update(['image' => $article->id]);
+        });
     }
 } 
